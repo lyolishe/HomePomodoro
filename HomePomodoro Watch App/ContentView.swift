7ef35659add77c00ed21877e2dcac4545b458ad8
= <=>
2026-04-11 15:28:00 +0300

    Initial Commit

---
+++ HomePomodoro Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HomePomodoro Watch App
+//
+//  Created by Леонид Попов on 11/04/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HomePomodoro Watch App/HomePomodoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomePomodoroApp.swift
+//  HomePomodoro Watch App
+//
+//  Created by Леонид Попов on 11/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct HomePomodoro_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
